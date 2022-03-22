56db319f3a237fa6b8cfa08542b52e72c5a3d7e8
Shaylin Chetty <shaylin.chetty@protonmail.com>
2022-03-22 18:16:15 +0200

    Using box to return a pass by value buffer

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,15 @@ extern {
     fn alert(s: &str);
 }
 
+//TODO: Return a byte slice - cannot return rust allocated mem
+// have to use this box because it needs to copy by value - generates a shim to create the buffer on the js side
 #[wasm_bindgen]
-pub fn greet() {
+pub fn generate_zip_blob() -> Box<[u8]> {
     let crc_calculator = BitwiseCrcCalculator {};
     let message = format!("CRC {}", crc_calculator.calculate_crc32());
     alert(&message[..]);
+
+    let ting: &[u8] = &[2, 3, 4, 5, 6, 6, 7, 8];
+
+    return Box::from(ting);
 }
