a1f39f5b6eba502b037d2b9bf74d95c9d4f5be73
Shaylin Chetty <shaylin.chetty@protonmail.com>
2022-03-30 09:05:23 +0200

    Added wrapper javascript to prepare the input directory

--- js/create_directory_mapping.js
+++ js/create_directory_mapping.js
@@ -2,21 +2,21 @@
 function create_directory_mapping(directory_listing, folder_prefix) {
     let directory_mapping = new Map();
 
-    for (const [key, value] of Object.entries(directory_listing)) {
-        const sub_value_name = get_sub_value_name(folder_prefix, key);
+    for (const [file_name, file_contents] of Object.entries(directory_listing)) {
+        const sub_file_name = get_sub_file_name(folder_prefix, file_name);
 
-        if (typeof value === "object") {
-            const sub_directory_mapping = create_directory_mapping(value, sub_value_name);
+        if (typeof file_contents === "object") {
+            const sub_directory_mapping = create_directory_mapping(file_contents, sub_file_name);
             directory_mapping = new Map([...directory_mapping, ...sub_directory_mapping])
         } else {
-            directory_mapping.set(sub_value_name, value);
+            directory_mapping.set(sub_file_name, file_contents);
         }
     }
 
     return directory_mapping;
 }
 
-function get_sub_value_name(folder_prefix, sub_directory) {
+function get_sub_file_name(folder_prefix, sub_directory) {
     if (folder_prefix) {
         return `${folder_prefix}/${sub_directory}`;
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 mod utils;
 mod crc;
 
-use wasm_bindgen::JsObject;
 use wasm_bindgen::prelude::*;
 use crc::bitwise_crc_calculator::BitwiseCrcCalculator;
 use crc::CrcCalculator;
+use js_sys::Object;
+use js_sys::Map;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
 #[cfg(feature = "wee_alloc")]
@@ -16,12 +17,17 @@ extern {
     fn alert(s: &str);
 }
 
+#[wasm_bindgen(module = "/js/create_directory_mapping.js")]
+extern "C" {
+    fn create_directory_mapping(directory_listing: Object, folder_prefix: String) -> Map;
+}
+
 //TODO: Return a boxed byte slice - cannot return rust allocated mem
 // have to use this box because it needs to copy by value - generates a shim to create the buffer on the js side
 #[wasm_bindgen]
-pub fn generate_zip_blob(zip_contents: &dyn JsObject) -> Box<[u8]> {
+pub fn generate_zip_blob(zip_contents: Object) -> Box<[u8]> {
     let crc_calculator = BitwiseCrcCalculator {};
-    let message = format!("CRC {}", zip_contents.keys());
+    let message = format!("CRC {:?}", Object::entries(&zip_contents));
     alert(&message[..]);
 
     let ting: &[u8] = &[2, 3, 4, 5, 6, 6, 7, 8];
