a8bcc3b1818f685602ee4ed304086a5ed05874ff
Shaylin Chetty <shaylin.chetty@protonmail.com>
2022-04-05 04:56:59 +0200

    Added test usage of crc calculator

--- src/lib.rs
+++ src/lib.rs
@@ -18,20 +18,16 @@ extern "C" {
     fn create_directory_mapping(directory_listing: &Object, folder_prefix: String) -> Map;
 }
 
-//TODO: Return a boxed byte slice - cannot return rust allocated mem
-// have to use this box because it needs to copy by value - generates a shim to create the buffer on the js side
 #[wasm_bindgen]
 pub fn generate_zip_blob(zip_contents: Object) -> Box<[u8]> {
     let directory_mapping = create_directory_mapping(&zip_contents, String::from(""));
-
-    // Maybe unwrap all keys and values and then give it to the blob size calculator to some stuff
 
     // 1. Determine the size of the zip that's going to be created in bytes
     // 2. Allocate the boxed slice for it on the heap+
     // 3. Start writing headers and files
 
     let crc_calculator = CrcCalculatorAdapter::new();
-    let message = format!("CRC {:?}", directory_mapping.get(&JsValue::from_str("capoo")));
+    let message = format!("Data {:?}, CRC {:?}", directory_mapping.get(&JsValue::from_str("capoo")), crc_calculator.calculate_crc32(b"capoo"));
     alert(&message[..]);
 
     let ting: &[u8] = &[2, 3, 4, 5, 6, 6, 7, 8];
