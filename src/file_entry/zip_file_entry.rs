58baf563fc148edc573b3788fffa2a9175d77739
Shaylin Chetty <shaylin.chetty@protonmail.com>
2022-04-24 13:02:18 +0200

    Completed central directory header generation tests

--- src/file_entry/zip_file_entry.rs
+++ src/file_entry/zip_file_entry.rs
@@ -4,6 +4,7 @@ pub struct ZipFileEntry {
     file_name: String,
     dos_time: u16,
     dos_date: u16,
+    entry_offset: u32,
 }
 
 impl ZipFileEntry {
@@ -17,12 +18,12 @@ impl ZipFileEntry {
     pub fn get_local_file_header(&self) -> Vec<u8> {
         let mut local_file_header: Vec<u8> = Vec::with_capacity(self.get_local_file_header_size());
 
-        let mut archive_details_header_section = self.get_archive_details_header_section();
+        let mut local_file_archive_details_section = self.get_local_file_archive_details_section();
         let mut modified_time_header_section = self.get_modified_time_header_section();
         let mut crc_and_size_header_section = self.get_crc_and_size_header_section();
         let mut local_file_name_header_section = self.get_local_file_name_header_section();
 
-        local_file_header.append(&mut archive_details_header_section);
+        local_file_header.append(&mut local_file_archive_details_section);
         local_file_header.append(&mut modified_time_header_section);
         local_file_header.append(&mut crc_and_size_header_section);
         local_file_header.append(&mut local_file_name_header_section);
@@ -30,15 +31,15 @@ impl ZipFileEntry {
         local_file_header
     }
 
-    fn get_archive_details_header_section(&self) -> Vec<u8> {
-        let metadata_header_section: Vec<u8> = vec![
+    fn get_local_file_archive_details_section(&self) -> Vec<u8> {
+        let archive_details_section: Vec<u8> = vec![
             0x50, 0x4B, 0x03, 0x04,     // local file header signature
             0x0A, 0x00,                 // version needed to extract
             0x00, 0x00,                 // general purpose bit flag
             0x00, 0x00,                 // compression method
         ];
 
-        metadata_header_section
+        archive_details_section
     }
 
     fn get_modified_time_header_section(&self) -> Vec<u8> {
@@ -88,16 +89,63 @@ impl ZipFileEntry {
         local_file_name_header_section
     }
 
-    fn get_central_directory_header_size(&self) -> usize {
+    pub fn get_central_directory_header_size(&self) -> usize {
         let base_header_size = 46;
 
         base_header_size + self.file_name.len()
     }
 
-    fn get_central_directory_header(&self) -> Vec<u8> {
+    pub fn get_central_directory_header(&self) -> Vec<u8> {
         let mut central_directory_header: Vec<u8> = Vec::with_capacity(self.get_central_directory_header_size());
 
+        let mut central_directory_archive_header_section = self.get_central_directory_archive_details_section();
+        let mut modified_time_header_section = self.get_modified_time_header_section();
+        let mut crc_and_size_header_section = self.get_crc_and_size_header_section();
+        let mut central_directory_details_header_section = self.get_central_directory_details_header_section();
+
+        central_directory_header.append(&mut central_directory_archive_header_section);
+        central_directory_header.append(&mut modified_time_header_section);
+        central_directory_header.append(&mut crc_and_size_header_section);
+        central_directory_header.append(&mut central_directory_details_header_section);
+
         central_directory_header
+    }
+
+    fn get_central_directory_archive_details_section(&self) -> Vec<u8> {
+        let archive_details_section: Vec<u8> = vec![
+            0x50, 0x4B, 0x01, 0x02,     // central directory header signature
+            0x3F, 0x00,                 // version made by
+            0x0A, 0x00,                 // version needed to extract
+            0x00, 0x00,                 // general purpose bit flag
+            0x00, 0x00,                 // compression method
+        ];
+
+        archive_details_section
+    }
+
+    fn get_central_directory_details_header_section(&self) -> Vec<u8> {
+        let mut name_header_section: Vec<u8> = Vec::with_capacity(18 + self.file_name.len());
+
+        let name_length = self.file_name.len() as u16;
+        let mut file_name_length = Vec::from(name_length.to_le_bytes());
+        let mut extra_field_length = vec![0x00, 0x00];
+        let mut file_comment_length = vec![0x00, 0x00];
+        let mut disk_number_start = vec![0x00, 0x00];
+        let mut internal_file_attributes = vec![0x00, 0x00];
+        let mut external_file_attributes = vec![0x00, 0x00, 0x00, 0x00];
+        let mut relative_offset = Vec::from(self.entry_offset.to_le_bytes());
+        let mut file_name = Vec::from(self.file_name.as_bytes());
+
+        name_header_section.append(&mut file_name_length);
+        name_header_section.append(&mut extra_field_length);
+        name_header_section.append(&mut file_comment_length);
+        name_header_section.append(&mut disk_number_start);
+        name_header_section.append(&mut internal_file_attributes);
+        name_header_section.append(&mut external_file_attributes);
+        name_header_section.append(&mut relative_offset);
+        name_header_section.append(&mut file_name);
+
+        name_header_section
     }
 }
 
@@ -113,6 +161,7 @@ mod tests {
             file_name: String::from("yow"),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let header_size = file_entry.get_local_file_header_size();
@@ -128,6 +177,7 @@ mod tests {
             file_name: String::from("Capoo The BugCat Makes His Move"),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let header_size = file_entry.get_local_file_header_size();
@@ -143,6 +193,7 @@ mod tests {
             file_name: String::from("Capoo"),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -158,6 +209,7 @@ mod tests {
             file_name: String::from("Capoo"),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -173,6 +225,7 @@ mod tests {
             file_name: String::from("Capoo"),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -188,6 +241,7 @@ mod tests {
             file_name: String::from("Capoo"),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -203,6 +257,7 @@ mod tests {
             file_name: String::from("Capoo"),
             dos_time: 0x5611,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -218,6 +273,7 @@ mod tests {
             file_name: String::from("Capoo"),
             dos_time: 0,
             dos_date: 0x88AC,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -233,6 +289,7 @@ mod tests {
             file_name: String::from("Capoo"),
             dos_time: 0,
             dos_date: 0x88AC,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -248,6 +305,7 @@ mod tests {
             file_name: String::from("Capoo"),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -263,6 +321,7 @@ mod tests {
             file_name: String::from("Capoo The BugCat"),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -278,6 +337,7 @@ mod tests {
             file_name: String::from("FoamCat/CafeIsGood.txt"),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -293,6 +353,7 @@ mod tests {
             file_name: String::from("FoamCat"),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
@@ -310,10 +371,333 @@ mod tests {
             file_name: given_file_name.clone(),
             dos_time: 0,
             dos_date: 0,
+            entry_offset: 0,
         };
 
         let local_file_header = file_entry.get_local_file_header();
 
         assert_eq!(given_file_name.into_bytes(), &local_file_header[30..local_file_header.len()]);
+    }
+
+    #[test]
+    fn central_directory_header_size_with_small_file_name() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("yo"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let header_size = file_entry.get_central_directory_header_size();
+
+        assert_eq!(header_size, 48);
+    }
+
+    #[test]
+    fn central_directory_header_size_with_large_file_name() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("This Is An Exceedingly Long File Name With Many Characters.txt"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let header_size = file_entry.get_central_directory_header_size();
+
+        assert_eq!(header_size, 108);
+    }
+
+    #[test]
+    fn central_directory_header_signature() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("This Is An Exceedingly Long File Name With Many Characters.txt"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x50, 0x4B, 0x01, 0x02], &central_directory_header[0..4]);
+    }
+
+    #[test]
+    fn central_directory_header_version_made_by() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Ba"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x3F, 0x00], &central_directory_header[4..6]);
+    }
+
+    #[test]
+    fn central_directory_header_version_needed_to_extract() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Ba"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x0A, 0x00], &central_directory_header[6..8]);
+    }
+
+    #[test]
+    fn central_directory_header_general_purpose_bit_flag() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Ba"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x00, 0x00], &central_directory_header[8..10]);
+    }
+
+    #[test]
+    fn central_directory_header_compression_method() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Ba"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x00, 0x00], &central_directory_header[10..12]);
+    }
+
+    #[test]
+    fn central_directory_header_modified_time() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Ba"),
+            dos_time: 0xFF84,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x84, 0xFF], &central_directory_header[12..14]);
+    }
+
+    #[test]
+    fn central_directory_header_modified_date() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Ba"),
+            dos_time: 0,
+            dos_date: 0xFEEB,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0xEB, 0xFE], &central_directory_header[14..16]);
+    }
+
+    #[test]
+    fn central_directory_header_crc() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0xBEAD1234,
+            file_name: String::from("Ba"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x34, 0x12, 0xAD, 0xBE], &central_directory_header[16..20]);
+    }
+
+    #[test]
+    fn central_directory_header_compressed_size() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Ba"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x05, 0x00, 0x00, 0x00], &central_directory_header[20..24]);
+    }
+
+    #[test]
+    fn central_directory_header_uncompressed_size() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3, 4, 5]),
+            crc: 0,
+            file_name: String::from("Foam Cat"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x0D, 0x00, 0x00, 0x00], &central_directory_header[24..28]);
+    }
+
+    #[test]
+    fn central_directory_header_file_name_length() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3, 4, 5]),
+            crc: 0,
+            file_name: String::from("Foam Cat"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x08, 0x00], &central_directory_header[28..30]);
+    }
+
+    #[test]
+    fn central_directory_header_extra_field_length() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3, 4, 5]),
+            crc: 0,
+            file_name: String::from("Foam Cat"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x00, 0x00], &central_directory_header[30..32]);
+    }
+
+    #[test]
+    fn central_directory_header_file_comment_length() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Foam"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x00, 0x00], &central_directory_header[32..34]);
+    }
+
+    #[test]
+    fn central_directory_header_disk_number_start() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Foam"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x00, 0x00], &central_directory_header[34..36]);
+    }
+
+    #[test]
+    fn central_directory_header_internal_file_attributes() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Foam"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x00, 0x00], &central_directory_header[36..38]);
+    }
+
+    #[test]
+    fn central_directory_header_external_file_attributes() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Foam"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x00, 0x00, 0x00, 0x00], &central_directory_header[38..42]);
+    }
+
+    #[test]
+    fn central_directory_header_relative_offset() {
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: String::from("Foam"),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0x7712AB32,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!([0x32, 0xAB, 0x12, 0x77], &central_directory_header[42..46]);
+    }
+
+    #[test]
+    fn central_directory_header_file_name() {
+        let given_name = String::from("Foam Cat Takes Over the World");
+
+        let file_entry = ZipFileEntry {
+            body: Box::from([1, 2, 3]),
+            crc: 0,
+            file_name: given_name.clone(),
+            dos_time: 0,
+            dos_date: 0,
+            entry_offset: 0x7712AB32,
+        };
+
+        let central_directory_header = file_entry.get_central_directory_header();
+
+        assert_eq!(given_name.into_bytes(), &central_directory_header[46..central_directory_header.len()]);
     }
 }
